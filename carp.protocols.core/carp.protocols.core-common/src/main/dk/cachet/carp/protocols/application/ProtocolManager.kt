52f3342391d72eb4e3d5faf9e66b8fb1760481d5
Whathecode <steven.jeuris@gmail.com>
2018-10-09 12:57:47 +0200

    Updated `ProtocolManager` to use DTO since it is an application service.
    This is according to best practices in DDD.

--- carp.protocols.core/carp.protocols.core-common/src/main/dk/cachet/carp/protocols/application/ProtocolManager.kt
+++ carp.protocols.core/carp.protocols.core-common/src/main/dk/cachet/carp/protocols/application/ProtocolManager.kt
@@ -5,56 +5,63 @@ import dk.cachet.carp.protocols.domain.*
 
 
 /**
- * Application service which allows managing (multiple versions of) [StudyProtocol]'s.
+ * Application service which allows managing (multiple versions of) [StudyProtocolSnapshot]'s,
+ * which can be instantiated locally through [StudyProtocol].
  */
 class ProtocolManager( val repository: StudyProtocolRepository )
 {
     /**
      * Add the specified study [protocol].
      *
-     * @param protocol The [StudyProtocol] to add.
+     * @param protocol The [StudyProtocolSnapshot] to add.
      * @param versionTag An optional label used to identify this first version of the [protocol]. "Initial" by default.
      * @throws IllegalArgumentException when the [protocol] already exists.
+     * @throws InvalidConfigurationError when [protocol] is invalid.
      */
-    fun add( protocol: StudyProtocol, versionTag: String = "Initial" )
+    fun add( protocol: StudyProtocolSnapshot, versionTag: String = "Initial" )
     {
-        repository.add( protocol, versionTag )
+        val initializedProtocol = StudyProtocol.fromSnapshot( protocol )
+        repository.add( initializedProtocol, versionTag )
     }
 
     /**
      * Store an updated version of the specified study [protocol].
      *
-     * @param protocol An updated version of a [StudyProtocol] already stored.
+     * @param protocol An updated version of a [StudyProtocolSnapshot] already stored.
      * @param versionTag An optional unique label used to identify this specific version of the [protocol]. The current date/time by default.
      * @throws IllegalArgumentException when the [protocol] is not yet stored in the repository or when the [versionTag] is already in use.
+     * @throws InvalidConfigurationError when [protocol] is invalid.
      */
-    fun update( protocol: StudyProtocol, versionTag: String = DateTime.now().toString() )
+    fun update( protocol: StudyProtocolSnapshot, versionTag: String = DateTime.now().toString() )
     {
-        repository.update( protocol, versionTag )
+        val initializedProtocol = StudyProtocol.fromSnapshot( protocol )
+        repository.update( initializedProtocol, versionTag )
     }
 
     /**
-     * Find the [StudyProtocol] with the specified [protocolName] owned by [owner].
+     * Find the [StudyProtocolSnapshot] with the specified [protocolName] owned by [owner].
      *
      * @param owner The owner of the protocol to return.
      * @param protocolName The name of the protocol to return.
      * @param versionTag The tag of the specific version of the protocol to return. The latest version is returned when not specified.
      * @throws IllegalArgumentException when the [owner], [protocolName], or [versionTag] does not exist.
      */
-    fun getBy( owner: ProtocolOwner, protocolName: String, versionTag: String? = null ): StudyProtocol
+    fun getBy( owner: ProtocolOwner, protocolName: String, versionTag: String? = null ): StudyProtocolSnapshot
     {
-        return repository.getBy( owner, protocolName, versionTag )
+        val protocol: StudyProtocol = repository.getBy( owner, protocolName, versionTag )
+        return protocol.getSnapshot()
     }
 
     /**
-     * Find all [StudyProtocol]'s owned by [owner].
+     * Find all [StudyProtocolSnapshot]'s owned by [owner].
      *
      * @throws IllegalArgumentException when the [owner] does not exist.
-     * @return This returns the last version of each [StudyProtocol] owned by the specified [owner].
+     * @return This returns the last version of each [StudyProtocolSnapshot] owned by the specified [owner].
      */
-    fun getAllFor( owner: ProtocolOwner ): Sequence<StudyProtocol>
+    fun getAllFor( owner: ProtocolOwner ): List<StudyProtocolSnapshot>
     {
-        return repository.getAllFor( owner )
+        val protocols: Sequence<StudyProtocol> = repository.getAllFor( owner )
+        return protocols.map { it.getSnapshot() }.toList()
     }
 
     /**
