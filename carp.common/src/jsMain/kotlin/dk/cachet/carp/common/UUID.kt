447f469d1089209716d0fc501baa5444c249d66d
Whathecode <steven.jeuris@gmail.com>
2019-09-06 16:19:01 +0200

    Add (best effort) UUID generation to JS runtime.
    This is added so that unit and integration tests which rely on ID generation can succeed.

--- carp.common/src/jsMain/kotlin/dk/cachet/carp/common/UUID.kt
+++ carp.common/src/jsMain/kotlin/dk/cachet/carp/common/UUID.kt
@@ -12,10 +12,17 @@ actual class UUID actual constructor( actual val stringRepresentation: String )
     {
         actual fun randomUUID(): UUID
         {
-            // We do not need to support UUID generation in JS, as this is client-side code.
-            // Furthermore, it does not seem like it can generate true UUIDs:
+            // It does not seem like JS can generate true UUIDs, but this is a best effort:
             // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-            throw UnsupportedOperationException( "JavaScript runtimes cannot generate true UUIDs." )
+            // Regardless, we do not need to support UUID generation in JS, as this is used as client-side code.
+            // The only real reason for providing a best effort implementation here is to be able to run unit tests.
+            val uuidString = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace( Regex( "[xy]" ) ) { match ->
+                val random = js( "Math.random() * 16 | 0" )
+                val char = if ( match.value == "x" ) random else js( "random & 0x3 | 0x8" )
+                char.toString( 16 ) as CharSequence
+            }
+
+            return UUID( uuidString )
         }
     }
 
