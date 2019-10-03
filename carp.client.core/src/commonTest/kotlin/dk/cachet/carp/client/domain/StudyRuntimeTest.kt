25ef3b42c86d01a07bc3ffa5f7e01e5326a77a9f
Steven Jeuris <steven.jeuris@gmail.com>
2019-10-03 10:34:00 +0200

    Renamed missed `deploymentManager` to `deploymentService` variables.

--- carp.client.core/src/commonTest/kotlin/dk/cachet/carp/client/domain/StudyRuntimeTest.kt
+++ carp.client.core/src/commonTest/kotlin/dk/cachet/carp/client/domain/StudyRuntimeTest.kt
@@ -12,11 +12,11 @@ class StudyRuntimeTest
 {
     @Test
     fun initialize_matches_requested_runtime() = runBlockingTest {
-        // Create a deployment manager which contains a 'smartphone study'.
-        val ( deploymentManager, deploymentStatus) = createStudyDeployment( createSmartphoneStudy() )
+        // Create a deployment service which contains a 'smartphone study'.
+        val ( deploymentService, deploymentStatus) = createStudyDeployment( createSmartphoneStudy() )
 
         val deviceRegistration = smartphone.createRegistration()
-        val runtime = StudyRuntime.initialize( deploymentManager, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
+        val runtime = StudyRuntime.initialize( deploymentService, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
 
         assertEquals( deploymentStatus.studyDeploymentId, runtime.studyDeploymentId )
         assertEquals( smartphone.roleName, runtime.device.roleName )
@@ -24,22 +24,22 @@ class StudyRuntimeTest
 
     @Test
     fun initialize_deploys_when_possible() = runBlockingTest {
-        // Create a deployment manager which contains a 'smartphone study'.
-        val ( deploymentManager, deploymentStatus) = createStudyDeployment( createSmartphoneStudy() )
+        // Create a deployment service which contains a 'smartphone study'.
+        val ( deploymentService, deploymentStatus) = createStudyDeployment( createSmartphoneStudy() )
 
         val deviceRegistration = smartphone.createRegistration()
-        val runtime = StudyRuntime.initialize( deploymentManager, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
+        val runtime = StudyRuntime.initialize( deploymentService, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
 
         assertTrue( runtime.isDeployed )
     }
 
     @Test
     fun initialize_does_not_deploy_when_depending_on_other_devices() = runBlockingTest {
-        // Create a deployment manager which contains a study where 'smartphone' depends on another master device.
-        val ( deploymentManager, deploymentStatus) = createStudyDeployment( createDependentSmartphoneStudy() )
+        // Create a deployment service which contains a study where 'smartphone' depends on another master device.
+        val ( deploymentService, deploymentStatus) = createStudyDeployment( createDependentSmartphoneStudy() )
 
         val deviceRegistration = smartphone.createRegistration()
-        val runtime = StudyRuntime.initialize( deploymentManager, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
+        val runtime = StudyRuntime.initialize( deploymentService, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
 
         assertFalse( runtime.isDeployed )
     }
@@ -47,9 +47,9 @@ class StudyRuntimeTest
     @Test
     fun tryDeployment_only_succeeds_after_dependent_devices_are_registered() = runBlockingTest {
         // Create a study runtime for a study where 'smartphone' depends on another master device ('deviceSmartphoneDependsOn').
-        val ( deploymentManager, deploymentStatus) = createStudyDeployment( createDependentSmartphoneStudy() )
+        val ( deploymentService, deploymentStatus) = createStudyDeployment( createDependentSmartphoneStudy() )
         val deviceRegistration = smartphone.createRegistration()
-        val runtime = StudyRuntime.initialize( deploymentManager, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
+        val runtime = StudyRuntime.initialize( deploymentService, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
 
         // Dependent devices are not yet registered.
         val status = runtime.tryDeployment()
@@ -57,7 +57,7 @@ class StudyRuntimeTest
         assertFalse( status.isDeployed )
 
         // Once dependent devices are registered, deployment succeeds.
-        deploymentManager.registerDevice( deploymentStatus.studyDeploymentId, deviceSmartphoneDependsOn.roleName, deviceSmartphoneDependsOn.createRegistration() )
+        deploymentService.registerDevice( deploymentStatus.studyDeploymentId, deviceSmartphoneDependsOn.roleName, deviceSmartphoneDependsOn.createRegistration() )
         val succeededStatus = runtime.tryDeployment()
         assertTrue( succeededStatus.isReadyForDeployment )
         assertTrue( succeededStatus.isDeployed )
@@ -66,9 +66,9 @@ class StudyRuntimeTest
     @Test
     fun creating_runtime_fromSnapshot_obtained_by_getSnapshot_is_the_same() = runBlockingTest {
         // Create a study runtime snapshot for the 'smartphone' in 'smartphone study'.
-        val ( deploymentManager, deploymentStatus) = createStudyDeployment( createSmartphoneStudy() )
+        val ( deploymentService, deploymentStatus) = createStudyDeployment( createSmartphoneStudy() )
         val deviceRegistration = smartphone.createRegistration()
-        val runtime = StudyRuntime.initialize( deploymentManager, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
+        val runtime = StudyRuntime.initialize( deploymentService, deploymentStatus.studyDeploymentId, smartphone.roleName, deviceRegistration )
         val snapshot = StudyRuntimeSnapshot.fromStudyRuntime( runtime )
 
         val serialized = snapshot.toJson()
