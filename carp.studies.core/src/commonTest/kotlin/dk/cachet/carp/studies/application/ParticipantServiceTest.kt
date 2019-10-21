37bef9385981e03c920f7a831ee1ff3d2f100821
Steven Jeuris <steven.jeuris@gmail.com>
2019-10-21 15:32:23 +0200

    Allow creating participants and including them in `Study`.

---
+++ carp.studies.core/src/commonTest/kotlin/dk/cachet/carp/studies/application/ParticipantServiceTest.kt
@@ -0,0 +1,39 @@
+package dk.cachet.carp.studies.application
+
+import dk.cachet.carp.common.*
+import dk.cachet.carp.studies.domain.Participant
+import dk.cachet.carp.test.runBlockingTest
+import kotlin.test.*
+
+
+/**
+ * Tests for implementations of [ParticipantService].
+ */
+interface ParticipantServiceTest
+{
+    fun createParticipantService(): ParticipantService
+
+
+    @Test
+    fun createParticipant_has_matching_studyId() = runBlockingTest {
+        val service: ParticipantService = createParticipantService()
+        val studyId = UUID.randomUUID()
+
+        val participant = service.createParticipant( studyId, EmailAddress( "test@test.com" ) )
+
+        assertEquals( studyId, participant.studyId )
+    }
+
+    @Suppress( "ReplaceAssertBooleanWithAssertEquality" )
+    @Test
+    fun createParticipant_with_same_studyId_and_email_returns_same_participant() = runBlockingTest {
+        val service: ParticipantService = createParticipantService()
+        val studyId = UUID.randomUUID()
+        val email = EmailAddress( "test@test.com" )
+
+        val p1: Participant = service.createParticipant( studyId, email )
+        val p2: Participant = service.createParticipant( studyId, email )
+
+        assertTrue( p1.id == p2.id )
+    }
+}
