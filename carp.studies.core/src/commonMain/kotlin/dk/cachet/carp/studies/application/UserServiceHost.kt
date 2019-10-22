e44f855d7a6aab050e0cef28dcf51bac0e45fb87
Steven Jeuris <steven.jeuris@gmail.com>
2019-10-22 17:11:58 +0200

    Implement `UserService.createParticipant()` and also test account creation.

--- carp.studies.core/src/commonMain/kotlin/dk/cachet/carp/studies/application/UserServiceHost.kt
+++ carp.studies.core/src/commonMain/kotlin/dk/cachet/carp/studies/application/UserServiceHost.kt
@@ -11,10 +11,29 @@ class UserServiceHost( private val repository: UserRepository ) : UserService
 {
     /**
      * Create a participant for the [Study] with the specified [studyId] and [Account] identified by [emailAddress].
-     * In case no [Account] is associated with the specified [emailAddress] yet, an invitation to register will be sent out.
+     * In case no [Account] is associated with the specified [emailAddress] yet, an invitation to register is sent out.
      */
     override suspend fun createParticipant( studyId: UUID, emailAddress: EmailAddress ): Participant
     {
-        TODO()
+        var account = repository.findAccountWithEmail( emailAddress )
+        var participant: Participant? = account?.studyParticipations?.firstOrNull { it.studyId == studyId }
+
+        // Create an unverified account if it does not yet exist and send invitation email.
+        if ( account == null )
+        {
+            account = Account( emailAddress )
+            repository.addAccount( account )
+
+            // TODO: Mark account as unverified and send out invitation email.
+        }
+
+        // Create and add participant if it does not yet exist.
+        if ( participant == null )
+        {
+            participant = Participant( studyId )
+            repository.addStudyParticipation( account.id, participant )
+        }
+
+        return participant
     }
 }
