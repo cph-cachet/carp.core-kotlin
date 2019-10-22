53a24227e571e8a4b8443d60668b216d508e5458
Steven Jeuris <steven.jeuris@gmail.com>
2019-10-22 14:47:24 +0200

    Add `UserRepository` and in memory stub to test ``UserServiceHost`.
    The `UserServiceHost` test currently still fails as it is not implemented.

---
+++ carp.studies.core/src/commonMain/kotlin/dk/cachet/carp/studies/domain/UserRepository.kt
@@ -0,0 +1,27 @@
+package dk.cachet.carp.studies.domain
+
+import dk.cachet.carp.common.UUID
+
+
+interface UserRepository
+{
+    /**
+     * Add a new [account] to the repository.
+     *
+     * @throws IllegalArgumentException when an [account] with the same ID or email address already exists.
+     */
+    fun addAccount( account: Account )
+
+    /**
+     * Add [participant] information of a study that an account with the given [accountId] should participate in.
+     *
+     * @param accountId The ID of the account which acts as a [Participant] in a study.
+     * @param participant The [Participant] information of a study.
+     */
+    fun addStudyParticipation( accountId: UUID, participant: Participant )
+
+    /**
+     * Get all participants included in a study for the given [studyId].
+     */
+    fun getParticipantsForStudy( studyId: UUID ): List<Participant>
+}
