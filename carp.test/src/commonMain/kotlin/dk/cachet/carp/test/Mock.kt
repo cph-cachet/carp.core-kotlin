fbe6e42a9567ce810ee17c187df09816aaa9a9f5
Steven Jeuris <steven.jeuris@gmail.com>
2019-11-07 15:59:26 +0100

    Add support for mocking suspend calls in `Mock`.

--- carp.test/src/commonMain/kotlin/dk/cachet/carp/test/Mock.kt
+++ carp.test/src/commonMain/kotlin/dk/cachet/carp/test/Mock.kt
@@ -14,11 +14,19 @@ abstract class Mock<TMock>
 
     fun <TReturn> trackCall( function: KFunction1<TMock, TReturn> )
         = trackCall( function.name )
+    fun <TReturn> trackSuspendCall( function: KSuspendFunction1<TMock, TReturn> )
+        = trackCall( function.name )
     fun <T1, TReturn> trackCall( function: KFunction2<TMock, T1, TReturn>, arg1: T1 )
+        = trackCall( function.name, arg1 as Any )
+    fun <T1, TReturn> trackSuspendCall( function: KSuspendFunction2<TMock, T1, TReturn>, arg1: T1 )
         = trackCall( function.name, arg1 as Any )
     fun <T1, T2, TReturn> trackCall( function: KFunction3<TMock, T1, T2, TReturn>, arg1: T1, arg2: T2 )
         = trackCall( function.name, arg1 as Any, arg2 as Any )
+    fun <T1, T2, TReturn> trackSuspendCall( function: KSuspendFunction3<TMock, T1, T2, TReturn>, arg1: T1, arg2: T2 )
+        = trackCall( function.name, arg1 as Any, arg2 as Any )
     fun <T1, T2, T3, TReturn> trackCall( function: KFunction4<TMock, T1, T2, T3, TReturn>, arg1: T1, arg2: T2, arg3: T3 )
+        = trackCall( function.name, arg1 as Any, arg2 as Any, arg3 as Any )
+    fun <T1, T2, T3, TReturn> trackSuspendCall( function: KSuspendFunction4<TMock, T1, T2, T3, TReturn>, arg1: T1, arg2: T2, arg3: T3 )
         = trackCall( function.name, arg1 as Any, arg2 as Any, arg3 as Any )
     private fun trackCall( functionName: String, vararg arguments: Any )
     {
@@ -28,11 +36,19 @@ abstract class Mock<TMock>
 
     fun <TReturn> wasCalled( function: KFunction1<TMock, TReturn> ) : Boolean
         = wasCalled( function.name )
+    fun <TReturn> wasSuspendCalled( function: KSuspendFunction1<TMock, TReturn> ) : Boolean
+        = wasCalled( function.name )
     fun <T1, TReturn> wasCalled( function: KFunction2<TMock, T1, TReturn>, arg1: T1 ): Boolean
+        = wasCalled( function.name, arg1 as Any )
+    fun <T1, TReturn> wasSuspendCalled( function: KSuspendFunction2<TMock, T1, TReturn>, arg1: T1 ): Boolean
         = wasCalled( function.name, arg1 as Any )
     fun <T1, T2, TReturn> wasCalled( function: KFunction3<TMock, T1, T2, TReturn>, arg1: T1, arg2: T2 ): Boolean
         = wasCalled( function.name, arg1 as Any, arg2 as Any )
+    fun <T1, T2, TReturn> wasSuspendCalled( function: KSuspendFunction3<TMock, T1, T2, TReturn>, arg1: T1, arg2: T2 ): Boolean
+        = wasCalled( function.name, arg1 as Any, arg2 as Any )
     fun <T1, T2, T3, TReturn> wasCalled( function: KFunction4<TMock, T1, T2, T3, TReturn>, arg1: T1, arg2: T2, arg3: T3 ): Boolean
+        = wasCalled( function.name, arg1 as Any, arg2 as Any, arg3 as Any )
+    fun <T1, T2, T3, TReturn> wasSuspendCalled( function: KSuspendFunction4<TMock, T1, T2, T3, TReturn>, arg1: T1, arg2: T2, arg3: T3 ): Boolean
         = wasCalled( function.name, arg1 as Any, arg2 as Any, arg3 as Any )
     private fun wasCalled( functionName: String, vararg expectedArguments: Any ): Boolean
     {
