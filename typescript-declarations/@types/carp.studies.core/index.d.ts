61f7e1c027284d77485bc7fa7d69322c5225e8d5
Steven Jeuris <steven.jeuris@gmail.com>
2020-03-05 21:33:04 +0100

    Add StudyStatus TypeScript declaration.

--- typescript-declarations/@types/carp.studies.core/index.d.ts
+++ typescript-declarations/@types/carp.studies.core/index.d.ts
@@ -1,7 +1,26 @@
 declare module 'carp.studies.core'
 {
     import { dk as cdk } from 'carp.common'
+    import DateTime = cdk.cachet.carp.common.DateTime
     import UUID = cdk.cachet.carp.common.UUID
+
+
+    namespace dk.cachet.carp.studies.domain
+    {
+        class StudyStatus
+        {
+            constructor( studyId: UUID, name: string, creationDate: DateTime, canDeployToParticipants: boolean, isLive: boolean )
+
+            static get Companion(): StudyStatus$Companion
+
+            readonly studyId: UUID
+            readonly name: string
+            readonly creationDate: DateTime
+            readonly canDeployToParticipants: boolean
+            readonly isLive: boolean
+        }
+        interface StudyStatus$Companion { serializer(): any }
+    }
 
 
     namespace dk.cachet.carp.studies.domain.users
--- typescript-declarations/tests/carp.studies.core.ts
+++ typescript-declarations/tests/carp.studies.core.ts
@@ -2,16 +2,20 @@ import { expect } from 'chai'
 import VerifyModule from './VerifyModule'
 
 import { dk as cdk } from 'carp.common'
+import DateTime = cdk.cachet.carp.common.DateTime
 import UUID = cdk.cachet.carp.common.UUID
 import { dk } from 'carp.studies.core'
 import StudyOwner = dk.cachet.carp.studies.domain.users.StudyOwner
+import StudyStatus = dk.cachet.carp.studies.domain.StudyStatus
 
 
 describe( "carp.studies.core", () => {
     it( "verify module declarations", async () => {
         const instances = [
             new StudyOwner(),
-            StudyOwner.Companion
+            StudyOwner.Companion,
+            new StudyStatus( UUID.Companion.randomUUID(), "Test", DateTime.Companion.now(), false, false ),
+            StudyStatus.Companion
         ]
 
         const moduleVerifier = new VerifyModule( 'carp.studies.core', instances )
