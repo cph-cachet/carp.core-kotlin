6b7f4fe930710ecc2919824671cf2b6651ba1562
Steven Jeuris <steven.jeuris@gmail.com>
2020-03-06 13:50:20 +0100

    Added `Participant` and `AssignParticipantDevices` TypeScript declarations.

--- typescript-declarations/tests/carp.studies.core.ts
+++ typescript-declarations/tests/carp.studies.core.ts
@@ -1,10 +1,19 @@
 import { expect } from 'chai'
 import VerifyModule from './VerifyModule'
 
+import { kotlin } from 'kotlin'
+import ArrayList = kotlin.collections.ArrayList
+import HashSet = kotlin.collections.HashSet
+import toSet = kotlin.collections.toSet_us0mfu$
 import { dk as cdk } from 'carp.common'
 import DateTime = cdk.cachet.carp.common.DateTime
 import UUID = cdk.cachet.carp.common.UUID
+import UsernameIdentity = cdk.cachet.carp.common.users.UsernameAccountIdentity
 import { dk } from 'carp.studies.core'
+import AssignParticipantDevices = dk.cachet.carp.studies.domain.users.AssignParticipantDevices
+import getAssignedParticipantIds = dk.cachet.carp.studies.domain.users.participantIds_nvx6bb$
+import getAssignedDeviceRoles = dk.cachet.carp.studies.domain.users.deviceRoles_nvx6bb$
+import Participant = dk.cachet.carp.studies.domain.users.Participant
 import StudyOwner = dk.cachet.carp.studies.domain.users.StudyOwner
 import StudyStatus = dk.cachet.carp.studies.domain.StudyStatus
 
@@ -12,6 +21,10 @@ import StudyStatus = dk.cachet.carp.studies.domain.StudyStatus
 describe( "carp.studies.core", () => {
     it( "verify module declarations", async () => {
         const instances = [
+            new AssignParticipantDevices( UUID.Companion.randomUUID(), toSet( [ "Test" ] ) ),
+            AssignParticipantDevices.Companion,
+            new Participant( new UsernameIdentity( "Test" ) ),
+            Participant.Companion,
             new StudyOwner(),
             StudyOwner.Companion,
             new StudyStatus( UUID.Companion.randomUUID(), "Test", DateTime.Companion.now(), false, false ),
@@ -20,6 +33,26 @@ describe( "carp.studies.core", () => {
 
         const moduleVerifier = new VerifyModule( 'carp.studies.core', instances )
         await moduleVerifier.verify()
+    } )
+
+
+    describe( "AssignParticipantDevices", () => {
+        it( "initializes deviceRoleNames as set", () => {
+            const uuid = UUID.Companion.randomUUID()
+            const deviceRoleNames = toSet( [ "Test" ] )
+            const assigned = new AssignParticipantDevices( uuid, deviceRoleNames )
+            expect( assigned.deviceRoleNames ).equals( deviceRoleNames )
+        } )
+
+        it( "getAssigned participantIds and devices works", () => {
+            const participant1 = UUID.Companion.randomUUID()
+            const participant2 = UUID.Companion.randomUUID()
+            const assigned1 = new AssignParticipantDevices( participant1, toSet( [ "Test" ] ) )
+            const assigned2 = new AssignParticipantDevices( participant2, toSet( [ "Test" ] ) )
+            const assignedGroup = new ArrayList( [ assigned1, assigned2 ] )
+            expect( getAssignedParticipantIds( assignedGroup ) ).instanceof( HashSet )
+            expect( getAssignedDeviceRoles( assignedGroup ) ).instanceof( HashSet )
+        } )
     } )
 
 
--- typescript-declarations/tests/kotlin.ts
+++ typescript-declarations/tests/kotlin.ts
@@ -2,11 +2,16 @@ import { expect } from 'chai'
 import VerifyModule from './VerifyModule'
 
 import { Long } from 'kotlin'
+import { kotlin } from 'kotlin'
+import HashSet = kotlin.collections.HashSet
+import toSet = kotlin.collections.toSet_us0mfu$
 
 
 describe( "kotlin", () => {
     it( "verify module declarations", async () => {
-        const instances = new Array<any>()
+        const instances = [
+            new HashSet()
+        ]
 
         const moduleVerifier = new VerifyModule( 'kotlin', instances )
         await moduleVerifier.verify()
@@ -19,6 +24,15 @@ describe( "kotlin", () => {
             const answerAsNumber = answer.toNumber()
     
             expect( answerAsNumber ).equals( 42 )
+        } )
+    } )
+
+
+    describe( "Set", () => {
+        it ( "can convert array to set", () => {
+            const elements = [ "One", "Two", "Three" ]
+            const set = toSet( elements )
+            expect( set ).is.not.undefined
         } )
     } )
 } )
