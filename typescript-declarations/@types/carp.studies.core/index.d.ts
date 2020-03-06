098c907af383f7d52019b662c151209dafb1fd4c
Steven Jeuris <steven.jeuris@gmail.com>
2020-03-06 13:50:20 +0100

    Added `Participant` and `AssignParticipantDevices` TypeScript declarations.

--- typescript-declarations/@types/carp.studies.core/index.d.ts
+++ typescript-declarations/@types/carp.studies.core/index.d.ts
@@ -1,8 +1,12 @@
 declare module 'carp.studies.core'
 {
+    import { kotlin } from 'kotlin'
+    import ArrayList = kotlin.collections.ArrayList
+    import HashSet = kotlin.collections.HashSet
     import { dk as cdk } from 'carp.common'
     import DateTime = cdk.cachet.carp.common.DateTime
     import UUID = cdk.cachet.carp.common.UUID
+    import AccountIdentity = cdk.cachet.carp.common.users.AccountIdentity
 
 
     namespace dk.cachet.carp.studies.domain
@@ -25,6 +29,32 @@ declare module 'carp.studies.core'
 
     namespace dk.cachet.carp.studies.domain.users
     {
+        class AssignParticipantDevices
+        {
+            constructor( participantId: UUID, deviceRoleNames: HashSet<string> )
+
+            static get Companion(): AssignParticipantDevices$Companion
+
+            readonly participantId: UUID
+            readonly deviceRoleNames: Set<string>
+        }
+        function participantIds_nvx6bb$( assignedGroup: ArrayList<AssignParticipantDevices> ): HashSet<UUID>
+        function deviceRoles_nvx6bb$( assignedGroup: ArrayList<AssignParticipantDevices> ): HashSet<string>
+        interface AssignParticipantDevices$Companion { serializer(): any }
+
+
+        class Participant
+        {
+            constructor( accountIdentity: AccountIdentity, id?: UUID )
+
+            static get Companion(): Participant$Companion
+
+            readonly accountIdentity: AccountIdentity
+            readonly id: UUID
+        }
+        interface Participant$Companion { serializer(): any }
+
+
         class StudyOwner
         {
             constructor( id?: UUID )
--- typescript-declarations/@types/kotlin/index.d.ts
+++ typescript-declarations/@types/kotlin/index.d.ts
@@ -6,4 +6,22 @@ declare module 'kotlin'
 
         toNumber(): number
     }
+
+
+    namespace kotlin.collections
+    {
+        class ArrayList<T>
+        {
+            constructor( array: T[] )
+
+            get_za3lpa$( index: number ): T
+        }
+        function toList_us0mfu$<T>( array: T[] ): ArrayList<T>
+
+        class HashSet<T>
+        {
+            contains_11rb$( element: T ): boolean
+        }
+        function toSet_us0mfu$<T>( array: T[] ): HashSet<T>
+    }
 }
