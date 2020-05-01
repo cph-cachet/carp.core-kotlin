e60981ae8686c38346ae2dfceb4cc2a0a91f5401
Steven Jeuris <steven.jeuris@gmail.com>
2020-05-01 18:56:51 +0200

    Add additional `TimeSpan` factory methods.

--- carp.common/src/commonMain/kotlin/dk/cachet/carp/common/TimeSpan.kt
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/TimeSpan.kt
@@ -18,6 +18,8 @@ data class TimeSpan(
     companion object
     {
         private const val MICROSECONDS_IN_MS: Long = 1000
+        private const val MICROSECONDS_IN_S: Long = MICROSECONDS_IN_MS * 1000
+        private const val MICROSECONDS_IN_M: Long = MICROSECONDS_IN_S * 60
 
         /**
          * A constant used to specify an infinite time 'interval'.
@@ -31,15 +33,37 @@ data class TimeSpan(
          * The value parameter is converted to microseconds, and that number of microseconds is used to initialize the new [TimeSpan].
          * Therefore, value will only be considered accurate to the nearest microsecond.
          */
-        fun fromMilliseconds( ms: Double ): TimeSpan
-        {
-            val ticks: Long = (ms * MICROSECONDS_IN_MS).toLong()
-            return TimeSpan( ticks )
-        }
+        fun fromMilliseconds( ms: Double ): TimeSpan = TimeSpan( (ms * MICROSECONDS_IN_MS).toLong() )
+
+        /**
+         * Returns a [TimeSpan] that represents a specified number of seconds.
+         *
+         * The value parameter is converted to microseconds, and that number of microseconds is used to initialize the new [TimeSpan].
+         * Therefore, value will only be considered accurate to the nearest microsecond.
+         */
+        fun fromSeconds( s: Double ): TimeSpan = TimeSpan( (s * MICROSECONDS_IN_S).toLong() )
+
+        /**
+         * Returns a [TimeSpan] that represents a specified number of minutes.
+         *
+         * The value parameter is converted to microseconds, and that number of microseconds is used to initialize the new [TimeSpan].
+         * Therefore, value will only be considered accurate to the nearest microsecond.
+         */
+        fun fromMinutes( m: Double ): TimeSpan = TimeSpan( (m * MICROSECONDS_IN_M).toLong() )
     }
 
     /**
      * Gets the value of the current [TimeSpan] expressed in whole and fractional milliseconds.
      */
     val totalMilliseconds: Double get() = this.microseconds.toDouble() / MICROSECONDS_IN_MS
+
+    /**
+     * Gets the value of the current [TimeSpan] expressed in whole and fractional seconds.
+     */
+    val totalSeconds: Double get() = this.microseconds.toDouble() / MICROSECONDS_IN_S
+
+    /**
+     * Gets the value of the current [TimeSpan] expressed in whole and fractional minutes.
+     */
+    val totalMinutes: Double get() = this.microseconds.toDouble() / MICROSECONDS_IN_M
 }
