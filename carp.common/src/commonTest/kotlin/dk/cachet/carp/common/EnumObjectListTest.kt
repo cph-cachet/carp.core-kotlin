72d255bf3e29bb8092a22ae9b6a0bb07be614fa1
Steven Jeuris <steven.jeuris@gmail.com>
2020-05-05 15:14:47 +0200

    Add test/example for `EnumObjectListTest`.

---
+++ carp.common/src/commonTest/kotlin/dk/cachet/carp/common/EnumObjectListTest.kt
@@ -0,0 +1,39 @@
+package dk.cachet.carp.common
+
+import kotlin.test.*
+
+
+/**
+ * Tests for [EnumObjectList].
+ */
+class EnumObjectListTest
+{
+    interface DataType { val typeName: String }
+
+    object GeolocationType : DataType
+    {
+        override val typeName: String = "geolocation"
+
+        fun create( longitude: Double, latitude: Double ) = Pair( longitude, latitude )
+    }
+
+    object SupportedTypes : EnumObjectList<DataType>()
+    {
+        val GEOLOCATION = add( GeolocationType )
+    }
+
+
+    @Test
+    fun can_enumerate_and_access_interface_members()
+    {
+        val supportedTypes = SupportedTypes.map { it.typeName }
+        assertEquals( listOf( GeolocationType.typeName ), supportedTypes )
+    }
+
+    @Test
+    fun can_access_type_specific_members()
+    {
+        val geolocation = SupportedTypes.GEOLOCATION.create( 42.0, 42.0 )
+        assertEquals( Pair( 42.0, 42.0 ), geolocation )
+    }
+}
