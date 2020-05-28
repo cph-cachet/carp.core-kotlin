09a2c63066cadbda2af8bf0d68e25d2626e63d09
Steven Jeuris <steven.jeuris@gmail.com>
2020-05-28 23:24:05 +0200

    Add `TimeOfDay`: time of the day according to a 24-hour clock

---
+++ carp.common/src/commonTest/kotlin/dk/cachet/carp/common/TimeOfDayTest.kt
@@ -0,0 +1,42 @@
+package dk.cachet.carp.common
+
+import dk.cachet.carp.common.serialization.createDefaultJSON
+import kotlin.test.*
+
+
+/**
+ * Tests for [TimeOfDay].
+ */
+class TimeOfDayTest
+{
+    @Test
+    fun cant_initialize_incorrect_TimeOfDay()
+    {
+        assertFailsWith<IllegalArgumentException> { TimeOfDay( -1 ) }
+        assertFailsWith<IllegalArgumentException> { TimeOfDay( 24 ) }
+        assertFailsWith<IllegalArgumentException> { TimeOfDay( 0, -1 ) }
+        assertFailsWith<IllegalArgumentException> { TimeOfDay( 0, 60 ) }
+        assertFailsWith<IllegalArgumentException> { TimeOfDay( 0, 0, -1 ) }
+        assertFailsWith<IllegalArgumentException> { TimeOfDay( 0, 0, 60 ) }
+    }
+
+    @Test
+    fun toString_is_ISO8601()
+    {
+        assertEquals( "12:00:00", TimeOfDay( 12 ).toString() )
+        assertEquals( "03:00:00", TimeOfDay( 3 ).toString() )
+        assertEquals( "12:34:56", TimeOfDay( 12, 34, 56 ).toString() )
+    }
+
+    @Test
+    fun can_serialize_and_deserialize_TimeOfDay_using_JSON()
+    {
+        val time = TimeOfDay( 12 )
+
+        val json = createDefaultJSON()
+        val serialized = json.stringify( TimeOfDay.serializer(), time )
+        val parsed = json.parse( TimeOfDay.serializer(), serialized )
+
+        assertEquals( time, parsed )
+    }
+}
