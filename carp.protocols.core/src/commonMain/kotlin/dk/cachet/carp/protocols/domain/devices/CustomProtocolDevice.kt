e611d9d00508e94b95de28b5b20e2fd59dbcd618
Steven Jeuris <steven.jeuris@gmail.com>
2020-06-10 22:47:58 +0200

    Support deploying custom protocol definitions to devices.
    This relies on passing a custom serialized protocol format to a `CustomProtocolTask` on a `CustomProtocoLDevice` which is triggered at the start of the study.

---
+++ carp.protocols.core/src/commonMain/kotlin/dk/cachet/carp/protocols/domain/devices/CustomProtocolDevice.kt
@@ -0,0 +1,23 @@
+package dk.cachet.carp.protocols.domain.devices
+
+import dk.cachet.carp.common.Trilean
+import dk.cachet.carp.protocols.domain.data.DataType
+import dk.cachet.carp.protocols.domain.data.SamplingConfiguration
+import dk.cachet.carp.protocols.domain.tasks.CustomProtocolTask
+import kotlinx.serialization.Serializable
+import kotlin.reflect.KClass
+
+
+/**
+ * A master device which uses a single [CustomProtocolTask] to determine how to run a study on the device.
+ */
+@Serializable
+data class CustomProtocolDevice( override val roleName: String ) :
+    MasterDeviceDescriptor<DefaultDeviceRegistration, DefaultDeviceRegistrationBuilder>()
+{
+    override val samplingConfiguration: Map<DataType, SamplingConfiguration> = emptyMap()
+
+    override fun createDeviceRegistrationBuilder(): DefaultDeviceRegistrationBuilder = DefaultDeviceRegistrationBuilder()
+    override fun getRegistrationClass(): KClass<DefaultDeviceRegistration> = DefaultDeviceRegistration::class
+    override fun isValidConfiguration( registration: DefaultDeviceRegistration ) = Trilean.TRUE
+}
