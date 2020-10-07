56eff903d6b246dbf9ba66586e162547ccec7f42
Steven Jeuris <steven.jeuris@gmail.com>
2020-10-07 16:15:38 +0200

    Bugfix: updated TS declarations to match new `kotlinx.serialization-json` module.

--- typescript-declarations/@types/kotlinx-serialization-kotlinx-serialization-core-jsLegacy/index.d.ts
+++ typescript-declarations/@types/kotlinx-serialization-kotlinx-serialization-core-jsLegacy/index.d.ts
@@ -6,13 +6,4 @@ declare module 'kotlinx-serialization-kotlinx-serialization-core-jsLegacy'
         function get_set_gekvwj$( serializer: any ): any
         function get_map_kgqhr1$( serializer: any ): any
     }
-
-    namespace kotlinx.serialization.json
-    {
-        class Json
-        {
-            encodeToString_tf03ej$( serializer: any, obj: any ): string
-            decodeFromString_awif5v$( deserializer: any, string: string ): any
-        }
-    }
 }
