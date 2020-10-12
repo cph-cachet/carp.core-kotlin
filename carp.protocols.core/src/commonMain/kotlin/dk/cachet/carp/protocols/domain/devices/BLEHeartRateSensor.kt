c70e4a43045c4745b3657d09fc57bded3689792a
Steven Jeuris <steven.jeuris@gmail.com>
2020-10-12 18:04:11 +0200

    Add heart rate measure to `BLEHeartRateSensor`.

--- carp.protocols.core/src/commonMain/kotlin/dk/cachet/carp/protocols/domain/devices/BLEHeartRateSensor.kt
+++ carp.protocols.core/src/commonMain/kotlin/dk/cachet/carp/protocols/domain/devices/BLEHeartRateSensor.kt
@@ -2,15 +2,35 @@ package dk.cachet.carp.protocols.domain.devices
 
 import dk.cachet.carp.common.Trilean
 import dk.cachet.carp.common.data.DataType
+import dk.cachet.carp.protocols.domain.sampling.DataTypeSamplingSchemeList
 import dk.cachet.carp.protocols.domain.sampling.SamplingConfiguration
+import dk.cachet.carp.protocols.domain.sampling.carp.HeartRateSamplingScheme
+import dk.cachet.carp.protocols.domain.tasks.measures.DataTypeMeasure
 import kotlinx.serialization.Serializable
 import kotlin.reflect.KClass
 
 
+/**
+ * A Bluetooth Low Energy (BLE) device which implements a GATT Heart Rate service (https://www.bluetooth.com/specifications/gatt/services/).
+ */
 @Serializable
-data class BLEHeartRateSensor( override val roleName: String )
-    : DeviceDescriptor<MACAddressDeviceRegistration, MACAddressDeviceRegistrationBuilder>()
+data class BLEHeartRateSensor( override val roleName: String ) :
+    DeviceDescriptor<MACAddressDeviceRegistration, MACAddressDeviceRegistrationBuilder>()
 {
+    companion object
+    {
+        /**
+         * Measure the number of heart contractions (beats) per minute
+         */
+        fun heartRate() = DataTypeMeasure( SamplingSchemes.HEARTRATE.type )
+    }
+
+    object SamplingSchemes : DataTypeSamplingSchemeList()
+    {
+        val HEARTRATE = add( HeartRateSamplingScheme ) // No configuration options available.
+    }
+
+
     override val supportedDataTypes: Set<DataType> = emptySet()
 
     override val samplingConfiguration: Map<DataType, SamplingConfiguration> = emptyMap()
