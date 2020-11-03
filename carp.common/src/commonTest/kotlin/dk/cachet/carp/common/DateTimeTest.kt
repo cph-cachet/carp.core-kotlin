56cdc7b8ad3f88a05c353a60bb7e4ff222baf4a4
Steven Jeuris <steven.jeuris@gmail.com>
2020-11-03 23:28:17 +0100

    Rename `DateTime.toString` to `defaultFormat` for consistency.
    We expect `toString` methods to be lossless.

--- carp.common/src/commonTest/kotlin/dk/cachet/carp/common/DateTimeTest.kt
+++ carp.common/src/commonTest/kotlin/dk/cachet/carp/common/DateTimeTest.kt
@@ -12,18 +12,18 @@ class DateTimeTest
     private val isoRegex = Regex( """\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d.\d{3}Z""" )
 
     @Test
-    fun toString_is_ISO8601_UTC()
+    fun defaultFormat_is_ISO8601_UTC()
     {
-        val nowString = DateTime.now().toString()
+        val nowString = DateTime.now().defaultFormat()
 
         val isIsoRepresentation = nowString.matches( isoRegex )
         assertTrue( isIsoRepresentation, "\"$nowString\" does not match the expected ISO format." )
     }
 
     @Test
-    fun toString_has_precision_of_3_decimal_fraction_digits()
+    fun defaultFormat_has_precision_of_3_decimal_fraction_digits()
     {
-        val utcZero = DateTime( 0 ).toString() // Ends in all zeros, so tailing zeros might be omitted.
+        val utcZero = DateTime( 0 ).defaultFormat() // Ends in all zeros, so tailing zeros might be omitted.
 
         val isIsoRepresentation = utcZero.matches( isoRegex )
         assertTrue( isIsoRepresentation, "\"$utcZero\" does not match the expected ISO format." )
