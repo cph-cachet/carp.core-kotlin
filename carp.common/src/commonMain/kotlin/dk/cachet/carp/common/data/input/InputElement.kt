16e390741b87933a8f1928eb8e8513699e7d2782
Steven Jeuris <steven.jeuris@gmail.com>
2020-11-22 02:11:56 +0100

    Start of `InputElement` abstraction.

---
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/data/input/InputElement.kt
@@ -0,0 +1,28 @@
+package dk.cachet.carp.common.data.input
+
+import kotlin.reflect.KClass
+
+
+/**
+ * Describes data and its constraints which may be input by a user.
+ */
+interface InputElement<TData : Any>
+{
+    /**
+     * Describes the data which may be input.
+     */
+    val name: String
+
+    /**
+     * Validates whether the provided [input] matches the constraints of this input element.
+     */
+    fun isValid( input: TData ): Boolean
+
+    /**
+     * Return the class information of the expected input data ([TData]).
+     */
+    fun getDataClass(): KClass<TData>
+}
+
+
+typealias AnyInputElement = InputElement<*>
---
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/data/input/element/SelectOne.kt
@@ -0,0 +1,22 @@
+package dk.cachet.carp.common.data.input.element
+
+import dk.cachet.carp.common.data.input.InputElement
+import kotlinx.serialization.Serializable
+import kotlin.reflect.KClass
+
+
+/**
+ * User needs to select one out of multiple [options].
+ */
+@Serializable
+data class SelectOne( override val name: String, val options: Set<String> ) : InputElement<String>
+{
+    init
+    {
+        require( options.isNotEmpty() ) { "At least one option needs to be specified." }
+    }
+
+    override fun isValid( input: String ): Boolean = input in options
+
+    override fun getDataClass(): KClass<String> = String::class
+}
