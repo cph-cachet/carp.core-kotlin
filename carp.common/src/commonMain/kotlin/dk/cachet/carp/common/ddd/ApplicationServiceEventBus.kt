80b196c2e65bdf093697b3dadc77ad1f443a3f70
Steven Jeuris <steven.jeuris@gmail.com>
2021-01-18 22:09:35 +0100

    Adapter for `EventBus` to publish events for single application service.

---
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/ddd/ApplicationServiceEventBus.kt
@@ -0,0 +1,50 @@
+package dk.cachet.carp.common.ddd
+
+import kotlin.reflect.KClass
+
+
+/**
+ * An event bus which can be used by an [ApplicationService] to publish events associated with its service
+ * and subscribe to any other events.
+ */
+class ApplicationServiceEventBus<
+    TApplicationService : ApplicationService<TApplicationService, TEvent>,
+    TEvent : IntegrationEvent<TApplicationService>
+>( private val serviceKlass: KClass<TApplicationService>, private val eventBus: EventBus )
+{
+    /**
+     * Publish the specified [event].
+     */
+    suspend fun publish( event: TEvent ) = eventBus.publish( serviceKlass, event )
+
+    /**
+     * Subscribe to events of [eventType] belonging to [applicationServiceKlass] and handle them using [handler].
+     */
+    suspend fun <
+        TOtherService : ApplicationService<TOtherService, TOtherServiceEvent>,
+        TOtherServiceEvent : IntegrationEvent<TOtherService>> subscribe(
+        applicationServiceKlass: KClass<TOtherService>,
+        eventType: KClass<TOtherServiceEvent>,
+        handler: (TOtherServiceEvent) -> Unit
+    ) = eventBus.subscribe( applicationServiceKlass, eventType, handler )
+}
+
+/**
+ * Subscribe to events of type [TEvent] on this [ApplicationServiceEventBus] and handle them using [handler].
+ */
+suspend inline fun <
+    reified TApplicationService : ApplicationService<TApplicationService, TEvent>,
+    reified TEvent : IntegrationEvent<TApplicationService>
+> ApplicationServiceEventBus<*, *>.subscribe( noinline handler: (TEvent) -> Unit ) =
+    this.subscribe( TApplicationService::class, TEvent::class, handler )
+
+
+/**
+ * Create an adapter for this [EventBus] which can only publish events associated with [serviceKlass].
+ */
+fun <
+    TApplicationService : ApplicationService<TApplicationService, TEvent>,
+    TEvent : IntegrationEvent<TApplicationService>
+> EventBus.createApplicationServiceAdapter(
+    serviceKlass: KClass<TApplicationService>
+): ApplicationServiceEventBus<TApplicationService, TEvent> = ApplicationServiceEventBus( serviceKlass, this )
