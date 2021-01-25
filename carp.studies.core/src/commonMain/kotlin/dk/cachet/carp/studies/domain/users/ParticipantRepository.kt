02f21423388a8e249b3cfa7718650a82f3937140
Steven Jeuris <steven.jeuris@gmail.com>
2021-01-25 19:07:13 +0100

    Store `Participant`s in `Recruitment` rather than directly in repo.

--- carp.studies.core/src/commonMain/kotlin/dk/cachet/carp/studies/domain/users/ParticipantRepository.kt
+++ carp.studies.core/src/commonMain/kotlin/dk/cachet/carp/studies/domain/users/ParticipantRepository.kt
@@ -10,18 +10,6 @@ import dk.cachet.carp.studies.domain.Recruitment
 interface ParticipantRepository
 {
     /**
-     * Adds a new [participant] for the study with [studyId] to the repository.
-     *
-     * @throws IllegalArgumentException when a participant with the specified ID already exists within the study.
-     */
-    suspend fun addParticipant( studyId: UUID, participant: Participant )
-
-    /**
-     * Returns the participants which were added to the study with the specified [studyId].
-     */
-    suspend fun getParticipants( studyId: UUID ): List<Participant>
-
-    /**
      * Add a new [Recruitment] to the repository.
      *
      * @throws IllegalArgumentException when a recruitment with the same studyId already exists.
@@ -41,9 +29,9 @@ interface ParticipantRepository
     suspend fun updateRecruitment( recruitment: Recruitment )
 
     /**
-     * Remove all data (participants and recruitment) for the study with [studyId].
+     * Remove all data (only recruitment for now) for the study with [studyId].
      *
-     * @return True when all data for the study was removed; false when no data for the study is present in the repository.
+     * @return True when data was removed; false when no data for the study is present in the repository.
      */
     suspend fun removeStudy( studyId: UUID ): Boolean
 }
--- carp.studies.core/src/commonTest/kotlin/dk/cachet/carp/studies/domain/users/ParticipantRepositoryTest.kt
+++ carp.studies.core/src/commonTest/kotlin/dk/cachet/carp/studies/domain/users/ParticipantRepositoryTest.kt
@@ -1,7 +1,6 @@
 package dk.cachet.carp.studies.domain.users
 
 import dk.cachet.carp.common.UUID
-import dk.cachet.carp.common.users.AccountIdentity
 import dk.cachet.carp.studies.domain.Recruitment
 import dk.cachet.carp.test.runSuspendTest
 import kotlin.test.*
@@ -14,27 +13,6 @@ interface ParticipantRepositoryTest
 {
     fun createRepository(): ParticipantRepository
 
-
-    @Test
-    fun adding_participant_and_retrieving_it_succeeds() = runSuspendTest {
-        val repo = createRepository()
-        val studyId = UUID.randomUUID()
-
-        val participant = Participant( AccountIdentity.fromUsername( "user" ) )
-        repo.addParticipant( studyId, participant )
-        val studyParticipants = repo.getParticipants( studyId )
-        assertEquals( participant, studyParticipants.single() )
-    }
-
-    @Test
-    fun addParticipant_fails_for_duplicate_participant_id() = runSuspendTest {
-        val repo = createRepository()
-        val studyId = UUID.randomUUID()
-        val participant = Participant( AccountIdentity.fromUsername( "user" ) )
-        repo.addParticipant( studyId, participant )
-
-        assertFailsWith<IllegalArgumentException> { repo.addParticipant( studyId, participant ) }
-    }
 
     @Test
     fun adding_recruitment_and_retrieving_it_succeeds() = runSuspendTest {
@@ -89,15 +67,12 @@ interface ParticipantRepositoryTest
     fun removeStudy_succeeds() = runSuspendTest {
         val repo = createRepository()
         val studyId = UUID.randomUUID()
-        val participant = Participant( AccountIdentity.fromUsername( "user" ) )
-        repo.addParticipant( studyId, participant )
         val recruitment = Recruitment( studyId )
         repo.addRecruitment( recruitment )
 
         val isRemoved = repo.removeStudy( studyId )
 
         assertTrue( isRemoved )
-        assertEquals( 0, repo.getParticipants( studyId ).size )
         assertNull( repo.getRecruitment( studyId ) )
     }
 
