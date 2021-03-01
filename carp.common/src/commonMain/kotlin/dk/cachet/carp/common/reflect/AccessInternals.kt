044353486ef4a6138df2e54f01613ff3ee492d66
Steven Jeuris <steven.jeuris@gmail.com>
2021-03-01 14:35:42 +0100

    Remove suppressed warnings due to detekt bug (some fixed in 1.16.0-RC2).

--- carp.common/src/commonMain/kotlin/dk/cachet/carp/common/reflect/AccessInternals.kt
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/reflect/AccessInternals.kt
@@ -9,6 +9,5 @@ internal expect object AccessInternals
     /**
      * Set the value of the field with the given [fieldName] on a given object ([onObject]) to [value].
      */
-    @Suppress( "UnusedPrivateMember" ) // TODO: Remove once detekt bug is fixed: https://github.com/detekt/detekt/issues/3415
     fun setField( onObject: Any, fieldName: String, value: Any )
 }
