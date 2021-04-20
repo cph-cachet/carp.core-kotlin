a835c9c838ed9e967408ba2d31468b55fc8ebb6a
Steven Jeuris <steven.jeuris@gmail.com>
2021-04-20 14:41:07 +0200

    Refactor: centralize application service reflection tests.

---
+++ carp.studies.core/src/jvmTest/kotlin/dk/cachet/carp/studies/application/StudiesServicesReflectionTest.kt
@@ -0,0 +1,7 @@
+package dk.cachet.carp.studies.application
+
+import dk.cachet.carp.common.test.application.ApplicationServicesReflectionTest
+
+
+class StudiesServicesReflectionTest :
+    ApplicationServicesReflectionTest( StudiesServicesReflectionTest::class.java.`package`.name )
