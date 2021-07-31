fc9decd0237d943236ed51c9a4c6c80ed1cbfa25
Steven Jeuris <steven.jeuris@gmail.com>
2021-07-31 20:48:11 +0200

    Added `DataStreamService` and host implementation.

---
+++ carp.data.core/src/commonTest/kotlin/dk/cachet/carp/data/application/DataStreamServiceMock.kt
@@ -0,0 +1,23 @@
+package dk.cachet.carp.data.application
+
+import dk.cachet.carp.common.application.UUID
+import dk.cachet.carp.test.Mock
+
+
+class DataStreamServiceMock(
+    val getDataStreamResult: DataStreamBatch = MutableDataStreamBatch()
+) : Mock<DataStreamService>(), DataStreamService
+{
+    override suspend fun appendToDataStreams( studyDeploymentId: UUID, batch: DataStreamBatch ) =
+        trackSuspendCall( DataStreamService::appendToDataStreams, studyDeploymentId, batch )
+
+    override suspend fun getDataStream(
+        dataStream: DataStreamId,
+        fromSequenceId: Long,
+        toSequenceIdInclusive: Long?
+    ): DataStreamBatch
+    {
+        trackSuspendCall( DataStreamService::getDataStream, dataStream, fromSequenceId, toSequenceIdInclusive )
+        return getDataStreamResult
+    }
+}
---
+++ carp.data.core/src/jvmTest/kotlin/dk/cachet/carp/data/application/DataStreamServicesReflectionTest.kt
@@ -0,0 +1,7 @@
+package dk.cachet.carp.data.application
+
+import dk.cachet.carp.common.test.application.ApplicationServicesReflectionTest
+
+
+class DataStreamServicesReflectionTest :
+    ApplicationServicesReflectionTest( DataStreamServicesReflectionTest::class.java.`package`.name )
