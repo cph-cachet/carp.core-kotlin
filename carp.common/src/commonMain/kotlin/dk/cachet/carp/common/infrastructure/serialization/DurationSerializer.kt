dd0f8de4671770330149c79290f6cc6e522d46e6
Steven Jeuris <steven.jeuris@gmail.com>
2021-09-20 12:51:03 +0200

    Serialize `Duration` using ISO standard.
    Closes #287

--- carp.common/src/commonMain/kotlin/dk/cachet/carp/common/infrastructure/serialization/DurationSerializer.kt
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/infrastructure/serialization/DurationSerializer.kt
@@ -1,20 +1,21 @@
 package dk.cachet.carp.common.infrastructure.serialization
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlin.time.Duration
 
 
 /**
  * Serializes [Duration] by converting it to microseconds.
- *
- * TODO: Once Kotlin 1.5.30 is released, it will be easy to serialize this as a String rather than Long:
- *   https://github.com/cph-cachet/carp.core-kotlin/issues/287
  */
-object DurationSerializer : KSerializer<Duration>
-    by createCarpLongPrimitiveSerializer(
-        {
-            if ( Duration.INFINITE.inWholeMicroseconds == it ) Duration.INFINITE
-            else Duration.microseconds( it )
-        },
-        { it.inWholeMicroseconds }
-    )
+object DurationSerializer : KSerializer<Duration> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor( "$NAMESPACE.${Duration::class.simpleName!!}", PrimitiveKind.STRING )
+
+    override fun deserialize( decoder: Decoder ): Duration = Duration.parseIsoString( decoder.decodeString() )
+    override fun serialize( encoder: Encoder, value: Duration ) = encoder.encodeString( value.toIsoString() )
+}
