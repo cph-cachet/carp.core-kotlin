0d96e5f94387ac1e5b30e6c3567871502f333546
Lars Toft Jacobsen <1213126+ltj@users.noreply.github.com>
2021-12-10 13:36:50 +0100

    apply restricted format to namespacedId name and namespace

--- carp.common/src/commonMain/kotlin/dk/cachet/carp/common/application/NamespacedId.kt
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/application/NamespacedId.kt
@@ -7,7 +7,11 @@ import kotlinx.serialization.Serializable
 
 /**
  * A uniquely named identifier within a [namespace].
- * TODO: This likely needs more restrictions in line with other standards. Disallow certain symbols? Lower-case only?
+ * The [namespace] format is lowercase alphanumeric (and underscores) words delimited by dots.
+ * Example: "tld.domain.subdomain"
+ *
+ * The [name] format is a single lowercase alphanumeric (and underscores) word.
+ * Example: "myname" or "my_name"
  */
 @Serializable( with = NamespacedIdSerializer::class )
 data class NamespacedId(
@@ -26,22 +30,25 @@ data class NamespacedId(
 {
     init
     {
-        require( namespace.isNotEmpty() ) { "Namespace needs to be set." }
-        require( !name.contains( '.' ) ) { "Name may not contain any periods. Periods are reserved for namespaces." }
+        require( namespaceRegex.matches( namespace ) )
+            { "Invalid namespace representation: expected lowercase alpha-numeric (underscore included) words delimited by dots." }
+        require( nameRegex.matches( name ) )
+            { "Invalid name representation: expected a single lowercase alpha-numeric (underscore included) word." }
     }
 
 
     companion object
     {
         /**
-         * Initializes a [NamespacedId] based on a string, formatted as: "<namespace>.<name>".
+         * Initializes a [NamespacedId] based on a string, formatted as: "<namespace>.<name>". It tries to be fairly
+         * lenient and will lowercase and trim the input.
          *
          * @throws IllegalArgumentException when no namespace is specified, i.e., [fullyQualifiedName] should contain at least one period.
          *   [name] will be set to the characters after the last period.
          */
         fun fromString( fullyQualifiedName: String ): NamespacedId
         {
-            val segments = fullyQualifiedName.split( '.' )
+            val segments = fullyQualifiedName.trim().lowercase().split( '.' )
             require( segments.count() > 1 ) { "A namespace needs to be specified." }
 
             val namespace = segments.subList( 0, segments.size - 1 ).joinToString( "." )
@@ -55,6 +62,18 @@ data class NamespacedId(
     override fun toString(): String = "$namespace.$name"
 }
 
+/**
+ * Regular expression to match a valid [NamespacedId.namespace] format.
+ * It must consist of one or more lowercase words, using alpha-numerics and underscores, delimited by dots.
+ * The namespace cannot end with a dot.
+ */
+val namespaceRegex = """^([a-z_0-9]+\.?)+[a-z_0-9]$""".toRegex()
+
+/**
+ * Regular expression to match a valid [NamespacedId.name] format.
+ * It must be a single lowercase word consisting only of alpha-numerics and underscores.
+ */
+val nameRegex = """^[a-z_0-9]+?$""".toRegex()
 
 /**
  * A custom serializer for [NamespacedId].
--- carp.common/src/commonTest/kotlin/dk/cachet/carp/common/application/NamespacedIdTest.kt
+++ carp.common/src/commonTest/kotlin/dk/cachet/carp/common/application/NamespacedIdTest.kt
@@ -27,6 +27,43 @@ class NamespacedIdTest
     }
 
     @Test
+    fun name_may_not_contain_non_alphanumerics()
+    {
+        assertFailsWith<IllegalArgumentException>
+        {
+            NamespacedId( "some.namespace", "contains-hyphen!" )
+        }
+    }
+
+    @Test
+    fun namespace_cannot_end_with_dot()
+    {
+        assertFailsWith<IllegalArgumentException>
+        {
+            NamespacedId("some.namespace.", "name")
+        }
+    }
+
+    @Test
+    fun namespace_cannot_start_with_dot()
+    {
+        assertFailsWith<IllegalArgumentException>
+        {
+            NamespacedId(".some.namespace", "name")
+        }
+    }
+
+    @Test
+    fun fromConstructor_succeeds()
+    {
+        val namespace = "some.namespace"
+        val name = "typename"
+        val fqName = NamespacedId(namespace, name)
+
+        assertEquals( "$namespace.$name", fqName.toString() )
+    }
+
+    @Test
     fun fromString_succeeds()
     {
         val namespace = "some.namespace"
@@ -35,6 +72,17 @@ class NamespacedIdTest
 
         assertEquals( namespace, fqName.namespace )
         assertEquals( name, fqName.name )
+    }
+
+    @Test
+    fun fromString_lenient_succeeds()
+    {
+        val namespace = " some.nameSpace"
+        val name = "typeName"
+        val fqName = NamespacedId.fromString( "$namespace.$name" )
+
+        assertEquals( "some.namespace", fqName.namespace )
+        assertEquals( "typename", fqName.name )
     }
 
     @Test
