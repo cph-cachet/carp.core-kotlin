caabd93b8f5aee4abe3e3de5e19bbe80d9422f42
Lars Jacobsen <1213126+ltj@users.noreply.github.com>
2021-12-18 15:30:01 +0100

    Only lowercase alphanumerics and underscore allowed in `NamespacedId`.
    NamespacedId format (#340)
    
    * apply restricted format to namespacedId name and namespace
    
    * add non-dashed uuid string format eg for use with namespacedId
    
    * Remove `toNonDashedString` and correct formatting.
    
    After some consideration, the `toNonDashedString` was removed for two reasons:
    - it seems like a premature abstraction. E.g. the caller may still want to decide to replace dashes with different characters. What to do with illegal characters is determined more by the place where they are considered illegal than by `UUID`. In this case, by `NamespacedId`.
    - it is redundant code that does not require a multiplatform expect/actual implementation. The only thing platform-specific is `randomUUID` which should probably be extracted somehow before including too much additional functionality into otherwise multiplatform `UUID` code.
    
    Co-authored-by: Steven Jeuris <steven.jeuris@gmail.com>

--- carp.common/src/commonMain/kotlin/dk/cachet/carp/common/application/NamespacedId.kt
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/application/NamespacedId.kt
@@ -7,27 +7,33 @@ import kotlinx.serialization.Serializable
 
 /**
  * A uniquely named identifier within a [namespace].
- * TODO: This likely needs more restrictions in line with other standards. Disallow certain symbols? Lower-case only?
  */
 @Serializable( with = NamespacedIdSerializer::class )
 data class NamespacedId(
     /**
      * Uniquely identifies the organization/person who determines how to interpret [name].
-     * To prevent conflicts, a reverse domain namespace is suggested: e.g., "org.openmhealth" or "dk.cachet.carp".
+     * The expected format is lowercase alphanumeric (and underscores) words delimited by dots.
+     * To prevent conflicts, a reverse domain namespace is suggested.
+     *
+     * Example: "tld.domain.subdomain", "org.openmhealth", or "dk.cachet.carp".
      */
     val namespace: String,
     /**
      * Uniquely identifies something within the [namespace].
-     *
+     * The expected format is a single lowercase alphanumeric (and underscores) word.
      * The name may not contain any periods. Periods are reserved for namespaces.
+     *
+     * Example: "myname" or "my_name".
      */
     val name: String
 )
 {
     init
     {
-        require( namespace.isNotEmpty() ) { "Namespace needs to be set." }
-        require( !name.contains( '.' ) ) { "Name may not contain any periods. Periods are reserved for namespaces." }
+        require( namespaceRegex.matches( namespace ) )
+            { "Invalid namespace representation: expected lowercase alpha-numeric (underscore included) words delimited by dots." }
+        require( nameRegex.matches( name ) )
+            { "Invalid name representation: expected a single lowercase alpha-numeric (underscore included) word." }
     }
 
 
@@ -35,13 +41,14 @@ data class NamespacedId(
     {
         /**
          * Initializes a [NamespacedId] based on a string, formatted as: "<namespace>.<name>".
+         * It is fairly lenient and will lowercase and trim the input.
          *
          * @throws IllegalArgumentException when no namespace is specified, i.e., [fullyQualifiedName] should contain at least one period.
          *   [name] will be set to the characters after the last period.
          */
         fun fromString( fullyQualifiedName: String ): NamespacedId
         {
-            val segments = fullyQualifiedName.split( '.' )
+            val segments = fullyQualifiedName.trim().lowercase().split( '.' )
             require( segments.count() > 1 ) { "A namespace needs to be specified." }
 
             val namespace = segments.subList( 0, segments.size - 1 ).joinToString( "." )
@@ -55,6 +62,18 @@ data class NamespacedId(
     override fun toString(): String = "$namespace.$name"
 }
 
+/**
+ * Regular expression to match a valid [NamespacedId.namespace] format.
+ * It must consist of one or more lowercase words, using alpha-numerics and underscores, delimited by dots.
+ * The namespace cannot end with a dot.
+ */
+val namespaceRegex = """^([a-z_0-9]+\.?)+[a-z_0-9]$""".toRegex()
+
+/**
+ * Regular expression to match a valid [NamespacedId.name] format.
+ * It must be a single lowercase word consisting only of alpha-numerics and underscores.
+ */
+val nameRegex = """^[a-z_0-9]+?$""".toRegex()
 
 /**
  * A custom serializer for [NamespacedId].
