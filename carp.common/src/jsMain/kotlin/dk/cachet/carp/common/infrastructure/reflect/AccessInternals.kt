645ac027f33e9ae6a1b7d7c472dfbc3c14911c04
Steven Jeuris <steven.jeuris@gmail.com>
2021-12-18 23:23:59 +0100

    Enable JS IR backend.

--- carp.common/src/jsMain/kotlin/dk/cachet/carp/common/infrastructure/reflect/AccessInternals.kt
+++ carp.common/src/jsMain/kotlin/dk/cachet/carp/common/infrastructure/reflect/AccessInternals.kt
@@ -5,9 +5,14 @@ internal actual object AccessInternals
 {
     actual fun setField( onObject: Any, fieldName: String, value: Any? )
     {
-        // Find the corresponding field name for the JavaScript runtime, which often attaches mangled suffixes.
+        // Find the corresponding field name for the JavaScript runtime:
+        // - seemingly includes 1 to 2 underscores on the JS IR runtime, likely to "hide" internals
+        // - often attaches mangled suffixes, maybe related to how suffixes are added to overloaded methods
         val fields = js( "Object.keys( onObject )" ) as Array<String>
-        val mangledField = fields.singleOrNull { it.startsWith( fieldName ) }
+        val mangledField = fields.singleOrNull {
+            it.startsWith( "_$fieldName" ) || it.startsWith( "__$fieldName" ) ||
+            it.startsWith( fieldName )
+        }
         checkNotNull( mangledField ) { "Could not find a matching field for \"$fieldName\" on the JavaScript runtime." }
 
         val toModify = onObject.asDynamic()
