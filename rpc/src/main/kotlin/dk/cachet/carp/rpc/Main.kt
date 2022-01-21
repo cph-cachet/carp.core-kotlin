f3300979bdf9ea2daa7273ccbf2461f956dd5982
Steven Jeuris <steven.jeuris@gmail.com>
2022-01-21 02:45:09 +0100

    `DataStreamServiceRequest` JSON schema, validated using generated examples.
    Part of the current schema will need to be split of in separate files to enable reuse in other schemas.
    For now, there is no requirement for all schemas to be available, but this should likely be added later.

--- rpc/src/main/kotlin/dk/cachet/carp/rpc/Main.kt
+++ rpc/src/main/kotlin/dk/cachet/carp/rpc/Main.kt
@@ -1,48 +1,65 @@
 package dk.cachet.carp.rpc
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.networknt.schema.JsonSchemaFactory
+import com.networknt.schema.SpecVersion
 import dk.cachet.carp.common.application.services.ApplicationService
 import kotlinx.serialization.Serializable
 import org.reflections.Reflections
 import java.io.File
 
 
-private const val NAMESPACE = "dk.cachet.carp"
-
-
 fun main( args: Array<String> )
 {
-    // Create example requests for all request objects and responses of application service methods.
-    val reflections = Reflections( NAMESPACE )
-    val serializableObjects = reflections.getTypesAnnotatedWith( Serializable::class.java )
-    val exampleRequests = reflections
+    // Find all application services.
+    val reflections = Reflections( ApplicationServiceInfo.NAMESPACE )
+    val appServices = reflections
         .getSubTypesOf( ApplicationService::class.java )
         .filter { it.isInterface }
+        .map { ApplicationServiceInfo( it ) }
+
+    // Create example requests for all request objects and responses of application service methods.
+    val serializableObjects = reflections.getTypesAnnotatedWith( Serializable::class.java )
+    val exampleRequests = appServices
         .associateWith { appService ->
-            val requestObjectName = "${appService.simpleName}Request"
+            val requestObjectName = appService.requestObjectName
             val requestObject = serializableObjects.single { it.simpleName == requestObjectName }
             checkNotNull( requestObject )
-                { "Could not find request object for ${appService.name}. Searched for: $requestObjectName" }
+                { "Could not find request object for ${appService.klass.name}. Searched for: $requestObjectName" }
 
-            generateExampleRequests( appService, requestObject )
+            generateExampleRequests( appService.klass, requestObject )
         }
+
+    // Validate available JSON schemas using the example requests.
+    val mapper = ObjectMapper()
+    for ( appService in appServices )
+    {
+        val requests = exampleRequests[ appService ]!!
+        val requestSchemaFile = File( appService.requestSchemaPath )
+        if ( !requestSchemaFile.exists() ) break
+
+        val requestSchema = JsonSchemaFactory.getInstance( SpecVersion.VersionFlag.V201909 )
+            .getSchema( requestSchemaFile.readText() )
+        requests.forEach {
+            val requestJson = mapper.readTree( it.requestObject.json )
+            val errors = requestSchema.validate( requestJson )
+            check( errors.isEmpty() )
+                { "JSON schema \"$requestSchemaFile\" does not match generated JSON example of \"${it.requestObject.klass.name}\": $errors" }
+        }
+    }
 
     // Output example requests to "build/rpc".
     val outputFolder = File( "build/rpc" )
     outputFolder.mkdirs()
     exampleRequests.forEach { (appService, examples) ->
-        val subsystem = Regex( Regex.escape( NAMESPACE ) + "\\.([^.]+)" )
-            .find( appService.`package`.name )
-            ?.groupValues?.lastOrNull()
-        checkNotNull( subsystem ) { "${appService.name} is residing in an unexpected namespace." }
-
-        val subsystemFolder = File( outputFolder, subsystem )
-        val serviceFolder = File( subsystemFolder, appService.simpleName )
+        val subsystemFolder = File( outputFolder, appService.subsystemName )
+        val serviceFolder = File( subsystemFolder, appService.klass.simpleName )
         serviceFolder.mkdirs()
 
         examples.forEach {
-            val requestFileName = "example-" + it.method.name + "-request.json"
+            val requestFileName = it.method.name + "-request-example.json"
             File( serviceFolder, requestFileName ).writeText( it.requestObject.json )
-            val responseFileName = "example-" + it.method.name + "-response.json"
+            val responseFileName = it.method.name + "-response-example.json"
             File( serviceFolder, responseFileName ).writeText( it.response.json )
         }
     }
