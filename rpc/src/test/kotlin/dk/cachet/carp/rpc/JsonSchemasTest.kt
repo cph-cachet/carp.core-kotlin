0554e98882c47a18925487479f7437d3664e1796
Steven Jeuris <steven.jeuris@gmail.com>
2022-01-21 18:29:52 +0100

    Refactor: split into multiple logically structured JSON schemas

--- rpc/src/test/kotlin/dk/cachet/carp/rpc/JsonSchemasTest.kt
+++ rpc/src/test/kotlin/dk/cachet/carp/rpc/JsonSchemasTest.kt
@@ -1,8 +1,10 @@
 package dk.cachet.carp.rpc
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.networknt.schema.JsonSchemaException
 import com.networknt.schema.JsonSchemaFactory
 import com.networknt.schema.SpecVersion
+import java.net.URI
 import kotlin.test.*
 
 
@@ -11,21 +13,23 @@ class JsonSchemasTest
     @Test
     fun schema_validation_for_generated_examples_succeeds()
     {
+        val schemaFactory = JsonSchemaFactory.getInstance( SpecVersion.VersionFlag.V201909 )
         val mapper = ObjectMapper()
 
         for ( (appService, requests) in exampleApplicationServiceRequests )
         {
-            // Find schema file, or skip in case no schema is defined yet.
-            val requestSchemaFile = appService.requestSchemaFile
-            if ( !requestSchemaFile.exists() ) break
+            // Find request schema, or skip in case no schema is defined yet.
+            val requestSchemaURI = URI( "file:${appService.requestSchemaPath}" )
+            val requestSchema =
+                try { schemaFactory.getSchema( requestSchemaURI ) }
+                catch ( _: JsonSchemaException ) { break }
 
-            val requestSchema = JsonSchemaFactory.getInstance( SpecVersion.VersionFlag.V201909 )
-                .getSchema( requestSchemaFile.readText() )
+            @Suppress( "UnreachableCode" ) // Seemingly a bug in detekt 1.18.1
             requests.forEach {
                 val requestJson = mapper.readTree( it.requestObject.json )
                 val errors = requestSchema.validate( requestJson )
                 check( errors.isEmpty() )
-                    { "JSON schema \"$requestSchemaFile\" does not match generated JSON example of \"${it.requestObject.klass}\": $errors" }
+                    { "JSON schema \"$requestSchema\" doesn't match generated JSON example of \"${it.requestObject.klass}\": $errors" }
             }
         }
     }
