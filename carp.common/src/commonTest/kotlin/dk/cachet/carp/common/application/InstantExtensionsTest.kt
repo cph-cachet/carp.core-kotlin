489ab62472914011e09079be8dee5dd14f93208a
Steven Jeuris <steven.jeuris@gmail.com>
2022-03-08 17:26:03 +0100

    Refactor: replace obsolete `Duration` API calls.

--- carp.common/src/commonTest/kotlin/dk/cachet/carp/common/application/InstantExtensionsTest.kt
+++ carp.common/src/commonTest/kotlin/dk/cachet/carp/common/application/InstantExtensionsTest.kt
@@ -2,7 +2,7 @@ package dk.cachet.carp.common.application
 
 import kotlinx.datetime.Instant
 import kotlin.test.*
-import kotlin.time.Duration
+import kotlin.time.Duration.Companion.microseconds
 
 
 class InstantExtensionsTest
@@ -14,7 +14,7 @@ class InstantExtensionsTest
         assertEquals( 1000, oneMillisecond.toEpochMicroseconds() )
 
         val oneMicrosecond = Instant.fromEpochMilliseconds( 0 )
-            .plus( Duration.microseconds( 1 ) )
+            .plus( 1.microseconds )
         assertEquals( 1, oneMicrosecond.toEpochMicroseconds() )
     }
 }
