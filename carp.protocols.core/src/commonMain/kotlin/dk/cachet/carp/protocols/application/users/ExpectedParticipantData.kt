d9a085f9b06d323cb78eb0231d796ed5dac19dc0
Steven Jeuris <steven.jeuris@gmail.com>
2022-03-30 20:07:43 +0200

    Support `inputBy` in `ParticipantGroup`.

--- carp.protocols.core/src/commonMain/kotlin/dk/cachet/carp/protocols/application/users/ExpectedParticipantData.kt
+++ carp.protocols.core/src/commonMain/kotlin/dk/cachet/carp/protocols/application/users/ExpectedParticipantData.kt
@@ -1,9 +1,6 @@
 package dk.cachet.carp.protocols.application.users
 
-import dk.cachet.carp.common.application.data.Data
-import dk.cachet.carp.common.application.data.input.CustomInput
 import dk.cachet.carp.common.application.data.input.InputDataType
-import dk.cachet.carp.common.application.data.input.InputDataTypeList
 import dk.cachet.carp.common.application.users.ParticipantAttribute
 import kotlinx.serialization.Serializable
 
@@ -76,33 +73,4 @@ fun Set<ExpectedParticipantData>.hasNoConflicts( exceptionOnConflict: Boolean = 
     }
 
     return noConflictingAttributes && noMultipleInputType
-}
-
-/**
- * Determines whether input [data] for a given [inputDataType],
- * as registered in [registeredInputDataTypes] or of type [CustomInput],
- * is expected to be input by the participant with role [inputByRole] (null if any) and is valid.
- */
-fun <TData : Data?> Set<ExpectedParticipantData>.isValidParticipantData(
-    registeredInputDataTypes: InputDataTypeList,
-    inputDataType: InputDataType,
-    data: TData,
-    /**
-     * The role of the participant who input [data]; null if any.
-     */
-    inputByRole: String? = null
-): Boolean
-{
-    val attributesForRole = this.filter {
-        when ( it.inputBy )
-        {
-            is ExpectedParticipantData.InputBy.Anyone -> true
-            is ExpectedParticipantData.InputBy.Roles -> inputByRole in it.inputBy.roleNames
-        }
-    }
-
-    val expectedData = attributesForRole.firstOrNull { it.inputDataType == inputDataType } ?: return false
-    val attribute = expectedData.attribute
-
-    return attribute.isValidData( registeredInputDataTypes, data )
 }
