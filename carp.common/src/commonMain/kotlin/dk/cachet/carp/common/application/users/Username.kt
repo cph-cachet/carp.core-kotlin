81d66834601a124061b453d68a09f34f1dce7b77
Steven Jeuris <steven.jeuris@gmail.com>
2022-04-04 12:40:17 +0200

    Serialize `Username` as string primitive.

--- carp.common/src/commonMain/kotlin/dk/cachet/carp/common/application/users/Username.kt
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/application/users/Username.kt
@@ -1,10 +1,21 @@
 package dk.cachet.carp.common.application.users
 
+import dk.cachet.carp.common.infrastructure.serialization.createCarpStringPrimitiveSerializer
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 
 
 /**
  * A unique name which identifies an account.
  */
-@Serializable
+@Serializable( UsernameSerializer::class )
 data class Username( val name: String )
+{
+    override fun toString(): String = name
+}
+
+
+/**
+ * A custom serializer for [Username].
+ */
+object UsernameSerializer : KSerializer<Username> by createCarpStringPrimitiveSerializer( { Username( it ) } )
