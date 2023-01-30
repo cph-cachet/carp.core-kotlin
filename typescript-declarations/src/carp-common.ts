d4ce6b5f0f5197c49d61e7aca478d0750bd9888c
Steven Jeuris <steven.jeuris@gmail.com>
2023-01-30 15:30:21 +0100

    Replace `any` with actual types for non-exported types with facades in generated TS
    Types for which no `@JsExport` is applied, when exposed in an API, are exported as `any`. For those types for which manual facades are written, replace these in the generated TypeScript sources with their facade type.

--- typescript-declarations/src/carp-common.ts
+++ typescript-declarations/src/carp-common.ts
@@ -1,9 +1,32 @@
 import * as extend from "carp.core-kotlin-carp.common"
+import * as kotlinStdLib from "./kotlin"
+import * as kotlinDateTime from "./kotlinx-datetime"
 
 
-// Augment internal types to implement desired base interfaces.
 declare module "carp.core-kotlin-carp.common"
 {
+    // Declare missing types for which no imports were generated.
+    namespace kotlin
+    {
+        type Long = kotlinStdLib.kotlin.Long
+    }
+    namespace kotlin.time
+    {
+        type Duration = kotlinStdLib.kotlin.time.Duration
+    }
+    namespace kotlin.collections
+    {
+        type List<T> = kotlinStdLib.kotlin.collections.List<T>
+        type Set<T> = kotlinStdLib.kotlin.collections.Set<T>
+        type Map<K, V> = kotlinStdLib.kotlin.collections.Map<K, V>
+    }
+    namespace kotlinx.datetime
+    {
+        type Instant = kotlinDateTime.kotlinx.datetime.Instant
+    }
+
+
+    // Augment internal types to implement desired base interfaces.
     namespace dk.cachet.carp.common.application.users
     {
         interface AccountIdentity {}
