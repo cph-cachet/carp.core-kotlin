26097f6e4edcc0cc2a137517440b7beac39ca506
Whathecode <steven.jeuris@gmail.com>
2023-02-19 20:01:11 +0100

    Refactor: data service logging using decorator

--- carp.data.core/src/jvmTest/kotlin/dk/cachet/carp/data/infrastructure/versioning/OutputDataStreamServiceTestRequests.kt
+++ carp.data.core/src/jvmTest/kotlin/dk/cachet/carp/data/infrastructure/versioning/OutputDataStreamServiceTestRequests.kt
@@ -1,10 +1,10 @@
 package dk.cachet.carp.data.infrastructure.versioning
 
-import dk.cachet.carp.common.infrastructure.services.SingleThreadedEventBus
+import dk.cachet.carp.common.infrastructure.services.createLoggedApplicationService
 import dk.cachet.carp.common.test.infrastructure.versioning.OutputTestRequests
 import dk.cachet.carp.data.application.DataStreamService
 import dk.cachet.carp.data.application.DataStreamServiceTest
-import dk.cachet.carp.data.infrastructure.DataStreamServiceLoggingProxy
+import dk.cachet.carp.data.infrastructure.DataStreamServiceDecorator
 import dk.cachet.carp.data.infrastructure.InMemoryDataStreamService
 
 
@@ -12,7 +12,15 @@ class OutputDataStreamServiceTestRequests :
     OutputTestRequests<DataStreamService>( DataStreamService::class ),
     DataStreamServiceTest
 {
-    override fun createService(): DataStreamService =
-        DataStreamServiceLoggingProxy( InMemoryDataStreamService(), SingleThreadedEventBus() )
-            .also { serviceLogger = it }
+    override fun createService(): DataStreamService
+    {
+        val (loggedService, logger) = createLoggedApplicationService(
+            InMemoryDataStreamService(),
+            ::DataStreamServiceDecorator
+        )
+
+        serviceLogger = logger
+
+        return loggedService
+    }
 }
