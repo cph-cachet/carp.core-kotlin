c35f6812ba79212bd25f4be8e0d559d4ac2f6420
Whathecode <steven.jeuris@gmail.com>
2023-11-05 16:14:41 +0100

    Fail on warnings for all compilations
    Warnings are now treated for all compilations, also test compilations, for which two fixes of missing `@Target` attributes were added.
    
    And, refactored warning configuration to be centralized in a single place.

--- carp.test/src/jsMain/kotlin/dk/cachet/carp/test/Nested.kt
+++ carp.test/src/jsMain/kotlin/dk/cachet/carp/test/Nested.kt
@@ -2,4 +2,5 @@ package dk.cachet.carp.test
 
 
 // The Mocha test runtime does not rely on a 'Nested' attribute in order to run tests of nested classes.
+@Target( AnnotationTarget.CLASS )
 actual annotation class Nested
