20066a69dd5b49bf065d133be0d713f4b19452ba
Daniel Jakab <jakdan99@gmail.com>
2024-04-12 10:40:09 +0200

    Add failing test for serializing unknown types using JS release
    The test is skipped for now.
    This only pertains to released sources. A similar test runs in `StudyProtocolSnapshotTest` targeting the JS platform, which passes. The "unknown polymorphic serializer" tries to access a field named `polymorphicDiscriminator`, which doesn't have this name after minification of the JS sources.

--- carp.common/src/commonMain/kotlin/dk/cachet/carp/common/infrastructure/reflect/AccessInternals.kt
+++ carp.common/src/commonMain/kotlin/dk/cachet/carp/common/infrastructure/reflect/AccessInternals.kt
@@ -3,6 +3,9 @@ package dk.cachet.carp.common.infrastructure.reflect
 
 /**
  * Provide access to runtime internals which cannot be accessed at compile time.
+ *
+ * Warning: on JavaScript targets, this only works for non-minified sources. Variables which aren't exported are mangled
+ * completely. So this should not be used in production code! It may still be useful for test automation.
  */
 internal expect object AccessInternals
 {
