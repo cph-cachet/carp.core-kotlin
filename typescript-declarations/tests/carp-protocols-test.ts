3dd692ad3c15849d9bf54711487a98f5c79a144b
Daniel Jakab <jakdan99@gmail.com>
2024-04-12 10:40:09 +0200

    Add failing test for serializing unknown types using JS release
    The test is skipped for now.
    This only pertains to released sources. A similar test runs in `StudyProtocolSnapshotTest` targeting the JS platform, which passes. The "unknown polymorphic serializer" tries to access a field named `polymorphicDiscriminator`, which doesn't have this name after minification of the JS sources.

--- typescript-declarations/tests/carp-protocols-test.ts
+++ typescript-declarations/tests/carp-protocols-test.ts
@@ -19,13 +19,25 @@ import ProtocolServiceRequest = protocols.infrastructure.ProtocolServiceRequest
 
 const serializedSnapshot = `{"id":"ad4ca03a-6f69-4a95-8701-488dc511925b","createdOn":"2023-01-30T21:02:56.068710100Z","version":0,"ownerId":"27879e75-ccc1-4866-9ab3-4ece1b735052","name":"Test protocol","description":"Test description","primaryDevices":[{"__type":"dk.cachet.carp.common.infrastructure.test.StubPrimaryDeviceConfiguration","isPrimaryDevice":true,"roleName":"Stub primary device"}],"connectedDevices":[{"__type":"dk.cachet.carp.common.infrastructure.test.StubDeviceConfiguration","roleName":"Stub device"},{"__type":"dk.cachet.carp.common.infrastructure.test.StubPrimaryDeviceConfiguration","isPrimaryDevice":true,"roleName":"Chained primary"},{"__type":"dk.cachet.carp.common.infrastructure.test.StubDeviceConfiguration","roleName":"Chained connected"}],"connections":[{"roleName":"Stub device","connectedToRoleName":"Stub primary device"},{"roleName":"Chained primary","connectedToRoleName":"Stub primary device"},{"roleName":"Chained connected","connectedToRoleName":"Chained primary"}],"tasks":[{"__type":"dk.cachet.carp.common.infrastructure.test.StubTaskConfiguration","name":"Task","measures":[{"__type":"dk.cachet.carp.common.application.tasks.Measure.DataStream","type":"dk.cachet.carp.stubpoint"}]}],"triggers":{"0":{"__type":"dk.cachet.carp.common.infrastructure.test.StubTriggerConfiguration","sourceDeviceRoleName":"Stub device"}},"taskControls":[{"triggerId":0,"taskName":"Task","destinationDeviceRoleName":"Stub primary device","control":"Start"}],"participantRoles":[{"role":"Role","isOptional":false},{"role":"Optional role","isOptional":true}],"assignedDevices":{"Stub primary device":["Role"]},"expectedParticipantData":[{"attribute":{"__type":"dk.cachet.carp.common.application.users.ParticipantAttribute.DefaultParticipantAttribute","inputDataType":"some.type"}},{"attribute":{"__type":"dk.cachet.carp.common.application.users.ParticipantAttribute.DefaultParticipantAttribute","inputDataType":"dk.cachet.carp.input.sex"},"assignedTo":{"__type":"dk.cachet.carp.common.application.users.AssignedTo.Roles","roleNames":["Role"]}}]}`
 
-
 describe( "carp-protocols-core", () => {
     describe( "StudyProtocolSnapshot", () => {
         it( "can deserialize", () => {
             const serializer = getSerializer( StudyProtocolSnapshot )
             const parsed = JSON.decodeFromString( serializer, serializedSnapshot )
             expect( parsed ).is.instanceOf( StudyProtocolSnapshot )
+        } )
+
+        it.skip( "can deserialize and serialize unknown types", () => {
+            const snapshotWithUnknownTypes = serializedSnapshot.replace(
+                "dk.cachet.carp.common.infrastructure.test.StubTaskConfiguration",
+                "com.unknown.CustomTaskConfiguration"
+            )
+            const serializer = getSerializer( StudyProtocolSnapshot )
+
+            const parsed = JSON.decodeFromString( serializer, snapshotWithUnknownTypes )
+            const serialized = JSON.encodeToString( serializer, parsed )
+            
+            expect( snapshotWithUnknownTypes ).equals( serialized )
         } )
 
         it( "can access snapshot properties", () => {
