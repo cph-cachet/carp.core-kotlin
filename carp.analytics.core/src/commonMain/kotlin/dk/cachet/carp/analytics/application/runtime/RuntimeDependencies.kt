c09285545830fb35f558796b6e7f61966ebf7361
Nigel Sjolin Grech <nigelgrech.ng@gmail.com>
2025-09-17 19:19:35 +0200

    feat(runtime): add RuntimeDependencies and RuntimeDependencyKey for dependency injection

---
+++ carp.analytics.core/src/commonMain/kotlin/dk/cachet/carp/analytics/application/runtime/RuntimeDependencies.kt
@@ -0,0 +1,18 @@
+package dk.cachet.carp.analytics.application.runtime
+
+import dk.cachet.carp.data.application.StudyDataService
+import dk.cachet.carp.analytics.application.data.DataRegistry
+
+/**
+ * Singleton object defining all supported runtime dependency keys for injection
+ * into workflow processes and services.
+ *
+ * These keys should be used to populate and query runtime injection maps.
+ */
+object RuntimeDependencies {
+    /** Key for injecting the StudyDataService. */
+    val StudyDataService = RuntimeDependencyKey<StudyDataService>("studyDataService")
+
+    /** Key for injecting the DataRegistry. */
+    val DataRegistry = RuntimeDependencyKey<DataRegistry>("dataRegistry")
+}
---
+++ carp.analytics.core/src/commonMain/kotlin/dk/cachet/carp/analytics/application/runtime/RuntimeDependencyKey.kt
@@ -0,0 +1,9 @@
+package dk.cachet.carp.analytics.application.runtime
+
+/**
+ * Represents a strongly-typed key used to access runtime dependencies from a dependency map.
+ *
+ * @param T The type of dependency associated with this key.
+ * @property name A human-readable identifier for the dependency (used for debugging/logging).
+ */
+data class RuntimeDependencyKey<T : Any>(val name: String)
