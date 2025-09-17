bb60534c9d03d639b6aaac655d0de45db3920686
Nigel Sjolin Grech <nigelgrech.ng@gmail.com>
2025-09-17 13:49:55 +0200

    feat(environment): add CommandGenerator and Environment interfaces for environment management

---
+++ carp.analytics.core/src/commonMain/kotlin/dk/cachet/carp/analytics/domain/environment/CommandGenerator.kt
@@ -0,0 +1,10 @@
+package dk.cachet.carp.analytics.domain.environment
+
+/**
+ * Interface for generating commands for setting up, activating, and tearing down environments.
+ */
+interface CommandGenerator {
+    fun generateSetupCommand(env: Environment): String
+    fun generateActivateCommand(env: Environment): String
+    fun generateTeardownCommand(env: Environment): String
+}
