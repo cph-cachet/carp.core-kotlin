a89cbc08551a433cd910c1a13d4ca2aaca168e99
Nigel Sjolin Grech <nigelgrech.ng@gmail.com>
2026-02-10 14:12:01 +1100

    refactor: remove unused import from ExternalProcess.kt

--- carp.analytics.core/src/commonMain/kotlin/dk/cachet/carp/analytics/domain/process/ExternalProcess.kt
+++ carp.analytics.core/src/commonMain/kotlin/dk/cachet/carp/analytics/domain/process/ExternalProcess.kt
@@ -1,6 +1,5 @@
 package dk.cachet.carp.analytics.domain.process
 
-import dk.cachet.carp.analytics.domain.execution.ExecutionContext
 
 /**
  * Defines the contract for externally executed processes (e.g., Python scripts, CLI tools).
